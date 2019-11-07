b58a9441d4ae4d435967e08c92530f8541225b76
Linh <lcahu91@gmail.com>
2019-11-06 19:29:29 -0800

    rm []

--- src/img_utils.py
+++ src/img_utils.py
@@ -19,7 +19,7 @@ def read_images_in_dir(datadir, img_height, img_width):
         path = os.path.join(datadir, i)
         img = cv2.imread(path, cv2.IMREAD_COLOR)
         img = cv2.resize(img, (img_height, img_width))
-        images.append([np.array(img)])
+        images.append(np.array(img))
     return images # returns list of resized images
 
 
