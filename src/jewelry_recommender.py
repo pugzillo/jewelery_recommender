8a19013f1208017f67637d39f21406e0d700c92c
Linh <lcahu91@gmail.com>
2019-11-06 17:43:38 -0800

    added CNN model

--- src/jewelry_recommender.py
+++ src/jewelry_recommender.py
@@ -16,9 +16,13 @@ Linh Chau
 train_dir = 'data/training/earrings'
 test_dir = 'data/testing/earrings'
 
+img_height = 256
+img_width = 256
+img_channel = 3 #RGB
+
 # Get images from the directories
-train_data = read_images_in_dir(train_dir)
-test_data = read_images_in_dir(test_dir)
+train_data = read_images_in_dir(train_dir, img_height, img_width)
+test_data = read_images_in_dir(test_dir, img_height, img_width)
 
 # Normalize the image pixels to 0-1
 trans_train_data = transform_images(train_data)
@@ -26,5 +30,15 @@ trans_test_data = transform_images(test_data)
 
 
 ### Setting up CNN Autoencoder Model
+autoencoder = Autoencoder()
+autoencoder.set_architecture(img_width, img_height, img_channel)
+autoencoder.compile_autoencoder()
+autoencoder.fit(train_data, test_data)
 
+## Encoded layer for both the train and test data
+encoded_train = autoencoder.predict(train_data)
+encoded_test = autoencoder.predict(test_data)
 
+#3 flatten the encoded img, so they are the shape (#imgs, height*width*channels of output of encoder)- input for KNN
+encoded_train_flat = encoded_train.reshape((-1, np.prod(encoded_train.shape[1:])))
+encoded_test_flat = encoded_test.reshape((-1, np.prod(encoded_test.shape[1:])))
