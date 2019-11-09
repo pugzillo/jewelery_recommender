874aca1d084e0e2d405b79c10a00afa37850433c
Linh <lcahu91@gmail.com>
2019-11-08 19:46:56 -0800

    def to make imagedatagenerator

--- src/img_utils.py
+++ src/img_utils.py
@@ -3,11 +3,12 @@ import numpy as np
 import os
 from tqdm import tqdm
 import skimage.io
+from keras.preprocessing.image import ImageDataGenerator
 
 """
 img_utils.py 
 
-Functions useful when reading in images for cnn autoencoder >> knn for image similarity retrival
+Functions useful when reading in images for cnn autoencoder >> knn for image similarity retrival and cnn classifier
 
 Linh Chau
 """
@@ -25,3 +26,17 @@ def read_images_in_dir(datadir, img_height, img_width):
 
 def transform_images(img_list):
     return np.array(img_list).astype('float32')/255.
+
+# takes in directory of images to create imagedatagenerator objects for cnn
+def image_to_imagedatagen(dir):
+    # rescales images
+    datagen = ImageDataGenerator(rescale=1./255)
+    #Creates generator object
+    generator = datagen.flow_from_directory(
+        dir,
+        target_size = (img_width, img_height),
+        batch_size = batch_size,
+        color_mode = 'rgb',
+        class_mode = class_mode
+    )
+    return(generator)
