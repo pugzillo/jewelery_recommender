5e5953ed2022b6709b9aad1823dd9afa3ed7a1f3
Linh <lcahu91@gmail.com>
2019-11-08 22:46:42 -0800

    added price filtering

--- jewelry_recommender.py
+++ jewelry_recommender.py
@@ -17,23 +17,28 @@ jewelry_recommender.py
 Linh Chau
 """
 
-### Processing Images 
+# Hyperparameter
 # Directories with Data
-train_dir = 'data/training/earrings'
-test_dir = 'data/testing/earrings'
+train_dir = 'data/training/necklaces'
+test_dir = 'data/testing/necklaces'
 
 img_height = 100
 img_width = 100
 img_channel = 3 #RGB
 
+price_limit = 500
+
+
+### Processing Images 
 # Get images from the directories
 logging.info(f"Reading and Processing Images from {train_dir}")
-train_data = read_images_in_dir(train_dir, img_height, img_width)
+train_data, train_ids = read_images_in_dir(train_dir, img_height, img_width)
 
 logging.info(f"Reading and Processing Images from {test_dir}")
-test_data = read_images_in_dir(test_dir, img_height, img_width)
+test_data, test_ids = read_images_in_dir(test_dir, img_height, img_width)
 
 # Normalize the image pixels to 0-1
+logging.info(f"Normalizing the images!")
 trans_train_data = transform_images(train_data)
 trans_test_data = transform_images(test_data)
 
@@ -56,17 +61,22 @@ encoded_train_flat = encoded_train.reshape((-1, np.prod(encoded_train.shape[1:])
 encoded_test_flat = encoded_test.reshape((-1, np.prod(encoded_test.shape[1:])))
 
 ## Save model in pickle
-pickle_name = 'jewelry_cnn_autoencoder_test.sav'
-pickle.dump(knn, open(pickle_name, 'wb'))
+pickle_names = ['jewelry_cnn_encoder_necklaces_test.pkl', 'jewelry_cnn_autoencoder_necklaces_test.pkl', 'jewelry_cnn_decoder_necklaces_test.pkl']
+autoencoder_models = [autoencoder.encoder, autoencoder.autoencoder, autoencoder.decoder]
 
+for model, pickles in zip(autoencoder_models, pickle_names):
+    pickle.dump(model, open(pickles, 'wb'))
 
 
 ##KNN to find items similar to a test image
 
 # Making Training set with the encoded training layer
 logging.info('Finding Similar items to input')
-knn = NearestNeighbors(n_neighbors=5, metric="cosine")
+knn = NearestNeighbors(n_neighbors=10, metric="cosine")
 knn.fit(encoded_train_flat)
+
+pickle_name = 'jewelry_knn_necklaces_test.pkl'
+pickle.dump(knn, open(pickle_name, 'wb'))
 
 test_img = 7
 plt.imshow(test_data[test_img])
@@ -74,14 +84,27 @@ plt.imshow(test_data[test_img])
 # Predict KNN for a test image
 distances, indices = knn.kneighbors(encoded_train_flat[test_img].reshape(1, -1))
 
-# print n_neighbors of the test image
-fig= plt.figure(figsize=(8, 8))
+# filters neighbors based off of prices
+logging.info('Price Filtering')
+product_data = pd.read_csv('data/product_metadata.csv') # training product metadata
+
+filtered_products, product_prices, product_urls = price_filter(indices[0], 
+                                                   train_ids, 
+                                                   product_data, 
+                                                   price_limit)
+
+# n_neighbors of the test image
+fig= plt.figure(figsize=(20, 20))
 columns = 3
 rows = 2
-for i, idx in zip(range(1, columns*rows +1), np.nditer(indices)):
+for i, idx in zip(range(1, columns*rows +1), filtered_products):
     fig.add_subplot(rows, columns, i)
-    plt.imshow(train_data[idx].reshape(256, 256, 3))
+    plt.imshow(train_data[idx].reshape(img_height, img_width, img_channel))
+    plt.title(f'Price: {product_prices[i]}')
 plt.show()
+
+
+
 
 
 
