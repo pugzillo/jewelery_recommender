3377eef5a8c4c39a05d540739af975db4a29258d
Linh <lcahu91@gmail.com>
2019-11-08 20:00:04 -0800

    get image ids

--- src/img_utils.py
+++ src/img_utils.py
@@ -16,12 +16,14 @@ Linh Chau
 # Reads in images in a directory; expects only imgs in a directory
 def read_images_in_dir(datadir, img_height, img_width):
     images = []
+    image_ids = []
     for i in tqdm(os.listdir(datadir)):
         path = os.path.join(datadir, i)
+        image_ids.append(re.split('[/.]', i)[3])
         img = cv2.imread(path, cv2.IMREAD_COLOR)
         img = cv2.resize(img, (img_height, img_width))
         images.append(np.array(img))
-    return images # returns list of resized images
+    return images, image_ids # returns list of resized images
 
 
 def transform_images(img_list):
