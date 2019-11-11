e932b878c496fc80cdc78a473f3f9353d0a5cf77
Linh <lcahu91@gmail.com>
2019-11-10 20:44:25 -0800

    def to read single image

--- src/img_utils.py
+++ src/img_utils.py
@@ -25,12 +25,17 @@ def read_images_in_dir(datadir, img_height, img_width):
         images.append(np.array(img))
     return images, image_ids 
 
+# Reads in a single image
+def read_novel_image(imagedir, img_height, img_width):
+    img = cv2.imread(imagedir, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (img_height, img_width))
+    return np.array(img)
 
 def transform_images(img_list):
     return np.array(img_list).astype('float32')/255.
 
 # takes in directory of images to create imagedatagenerator objects for cnn
-def image_to_imagedatagen(dir):
+def image_to_imagedatagen(dir, img_width = 100, img_height = 100, batch_size = 32, class_mode=None, shuffle=False):
     # rescales images
     datagen = ImageDataGenerator(rescale=1./255)
     #Creates generator object
@@ -39,7 +44,8 @@ def image_to_imagedatagen(dir):
         target_size = (img_width, img_height),
         batch_size = batch_size,
         color_mode = 'rgb',
-        class_mode = class_mode
+        class_mode = class_mode,
+        shuffle = shuffle
     )
     return(generator)
 
